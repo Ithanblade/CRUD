4ae1ce160aef790e899ebae8623b993d27ac3ba7
Ithan Camacho <ithan.camacho@epn.edu.ec>
2024-07-18 07:52:33 -0500

    Update

--- src/Main.java
+++ src/Main.java
@@ -13,24 +13,15 @@ public class Main {
         try (Connection connection= DriverManager.getConnection(url,user,password)){
 
             System.out.println("Conectado a ala base de datos");
-            String sql="update estudiantes set b1 = ? where cedulo = ?";
+            String sql="update estudiantes set b1 = ? where cedula = ?";
 
             pstmt = connection.prepareStatement(sql);
-            pstmt.setInt(1,19);
+            pstmt.setInt(1,12);
             pstmt.setString(2,"0052717064");
             pstmt.executeUpdate();
+            int n=pstmt.executeUpdate();
+            System.out.println("Se modificaron: "+n+" lineas");
 
-            while (resultSet.next()){
-
-                float prom=(resultSet.getFloat("b1") + resultSet.getFloat("b2"))/2;
-
-                System.out.println(resultSet.getString("cedula"));
-                System.out.println("Nombre: "+resultSet.getString("nombre"));
-                System.out.println("Cedula: "+resultSet.getString("cedula"));
-                System.out.println("Nota b1: "+resultSet.getString("b1"));
-                System.out.println("Nota b2: "+resultSet.getString("b2"));
-
-            }
 
         } catch (SQLException e1) {
             System.out.println(e1);

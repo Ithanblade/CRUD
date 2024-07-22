9a4d7811d6dafdb4ab53a039f974cb82ec12e1be
IthanC <ithan.camacho@epn.edu.ec>
2024-07-21 20:12:59 -0500

    coneccion a la base mongodb en la nube

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,26 @@
-
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.result.UpdateResult;
+import com.mongodb.client.result.DeleteResult;
 import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://localhost:3306/estudiantes2024";
-        String user = "root";
-        String password = "123456";
-        PreparedStatement pstmt = null;
-        Connection conn = null;
 
-
-        try {
-            conn = DriverManager.getConnection(url, user, password);
-
-            System.out.println("Conectado a ala base de datos");
-            String sql="update estudiantes set b1 = ? where cedula = ?";
-
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setInt(1,12);
-            pstmt.setString(2,"0052717064");
-            pstmt.executeUpdate();
-            int n=pstmt.executeUpdate();
-            System.out.println("Se modificaron: "+n+" lineas");
-
-
-        } catch (SQLException e1) {
-            System.out.println(e1);
-        }finally {
-            //cerrar la conexion con la base
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-                if (conn != null) {
-                    conn.close();
-                }
-            }catch (SQLException e) {
-                System.out.println(e.getMessage());
-            }
+        try (MongoClient mongoClient = MongoClients.create("mongodb+srv://ithancamacho:epn123@cluster0.uhwmdtv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")) {
+            MongoDatabase database = mongoClient.getDatabase("miBaseDeDatos");
+            MongoCollection<Document> collection = database.getCollection("miColeccion");
+            Document documento = new Document("nombre", "Damian")
+                    .append("apellido", "Rueda")
+                    .append("edad", 19);
+            collection.insertOne(documento);
+            System.out.println("Documento insertado con éxito");
 
         }
-
     }
 }
